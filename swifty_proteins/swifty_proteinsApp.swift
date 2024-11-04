d81b1d65914bc68cc425bd5cb4ca59a39d01a868
Anisa Kapateva <anisa56695@gmail.com>
2024-11-04 09:58:23 +0100

    Initial Commit

---
+++ swifty_proteins/swifty_proteinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_proteinsApp.swift
+//  swifty_proteins
+//
+//  Created by Anisa Kapateva on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct swifty_proteinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swifty_proteinsTests/swifty_proteinsTests.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_proteinsTests.swift
+//  swifty_proteinsTests
+//
+//  Created by Anisa Kapateva on 04.11.2024.
+//
+
+import Testing
+@testable import swifty_proteins
+
+struct swifty_proteinsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
