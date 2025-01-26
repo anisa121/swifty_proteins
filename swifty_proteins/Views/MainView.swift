add78705707afc1360e2e67c60a634ee66209065
Stepan Nikitin <nikitinste@gmail.com>
2025-01-27 00:18:00 +0100

    Legand list search and detail view nav title

--- swifty_proteins/Views/MainView.swift
+++ swifty_proteins/Views/MainView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct MainView: View {
     @StateObject private var viewModel = MainViewModel()
-    
+    @State private var searchText = ""
+
     var body: some View {
         NavigationStack {
-            List(viewModel.items) { item in
+            List(searchResults) { item in
                 NavigationLink(destination: DetailView(modelId: item.id, ligandName: item.name)) {
                     Text(item.name)
                 }
             }
             .navigationTitle("Ligands List")
         }
+        .searchable(text: $searchText)
+    }
+
+    var searchResults: [LigandName] {
+        if searchText.isEmpty {
+            return viewModel.items
+        }
+        return viewModel.items.filter { $0.name.contains(searchText) }
     }
 }
 
